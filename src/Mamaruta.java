344d8c2424e1b35f26e5f46d98de2aa11a8a5814
OVI <OVI@DESKTOP-1I3LNP3>
2020-11-09 22:10:29 +0200

    Lab + tema

---
+++ src/Mamaruta.java
@@ -0,0 +1,16 @@
+
+public class Mamaruta extends Gindac{
+	private static int a = culoare();
+	private int b = varsta();
+	
+	
+	
+	public static int culoare() {
+		System.out.println("Culoare Mamaruta");
+		return 2;
+	}
+	public int varsta() {
+		System.out.println("Varsta Mamaruta");
+		return 2;
+	}
+}
---
+++ src/Serpisor.java
@@ -0,0 +1,16 @@
+
+public class Serpisor extends Insecta {
+	private static int a = specia();
+	private static int b = zona();
+	
+	public static int specia() {
+		System.out.println("Python");
+		return 7;
+	}
+	public static int zona() {
+		System.out.println("Apuseni!");
+		return 20;
+		
+	}
+
+}
